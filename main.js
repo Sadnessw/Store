5928a5ba6e3bb2903ce3e21f74569d77185f9f33
Anton <sadnessbiblethump@gmail.com>
2019-07-12 17:01:39 +0300

    Create a products with icons and id via setIcon()

--- main.js
+++ main.js
@@ -1,22 +1,19 @@
 const images = document.querySelectorAll('li a img');
-
+const catalog = document.getElementsByClassName('catalog')[0];
 
 fetch('https://speisekarte.telenorma.info/api/shops/Damfastore%20Magdeburg/manufacturers?sort=1')
-.then(res => res.json())
-.then(json => setIcon(json))
-.catch(rej => console.log(`o_O ${rej}`));
+  .then(res => res.json())
+  .then(json => setIcon(json))
+  .catch(rej => console.log(`o_O ${rej}`));
 
 
 function setIcon(obj) {
   const withIcon = obj.filter(el => el.icon != null);
-  const newLi = document.createElement('li');
-  newLi.className = "item-catalog";
-  newLi.innerHTML = '<a href="#"><img alt="logo.png" width="200" height="200"></a>';
-  console.log(newLi)
-  for(let i = 0; i < withIcon.length; i++) { 
-
-    // images[i].setAttribute('src', withIcon[i].icon);
-    // images[i].setAttribute('id', obj._id)
+  for (let i = 0; i < withIcon.length; i++) {
+    const newLi = document.createElement('li');
+    newLi.className = "item-catalog";
+    newLi.id = withIcon[i]._id;
+    newLi.innerHTML = `<a href="#"><img src="${withIcon[i].icon}" alt="logo.png" width="200" height="200"></a>`;
+    catalog.appendChild(newLi);
   }
-  console.log(withIcon)
 }
